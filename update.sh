b6418515df29b73172bfc6b7e4bed1e224ed2024
Harry de Boer <hldeboer@gmail.com>
2024-07-15 09:57:01 +0200

    update update

--- update.sh
+++ update.sh
@@ -11,4 +11,13 @@ docker compose pull
 docker compose up -d
 docker cp /var/www/letsencrypt liedjesuitnederland:/etc
 docker compose restart web
+PREFIX="docker exec -it --user=www-data liedjesuitnederland"
+if $PREFIX sh -c "test ! -d .git"
+then
+  docker cp /var/www/liedjesuitnederland.nl/. liedjesuitnederland:/var/www/html
+  docker cp /var/www/.ssh liedjesuitnederland:/var/www
+else
+  $PREFIX git pull origin master
+  docker cp /var/www/liedjesuitnederland.nl/.env.local liedjesuitnederland:/var/www/html/.env.local
+fi
 docker system prune -f
