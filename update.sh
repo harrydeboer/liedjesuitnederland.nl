fea7f2d6f421bba0aeae1651e6e6629124941508
Harry de Boer <hldeboer@gmail.com>
2024-07-17 14:50:54 +0200

    update as root

--- update.sh
+++ update.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -e
 
-if [[ $EUID -eq 0 ]]; then
-  echo "This script must NOT be run as root" 1>&2
-  exit 1
-fi
 docker login --username harrydeboer
-git pull origin master
+sudo -u www-data git pull origin master
 docker compose pull
 docker compose up -d
 docker cp /var/www/letsencrypt/certs/archive/liedjesuitnederland.nl/fullchain1.pem liedjesuitnederland:/usr/local/lsws/admin/conf/webadmin.crt
